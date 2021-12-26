5683d427510f8e7f57d4697c73969bc1ed63103f
DongJun Shin <shindj4613@outlook.com>
2021-12-26 23:54:10 +0900

    end of useEffect

--- react-example/src/App.js
+++ react-example/src/App.js
@@ -1,17 +1,36 @@
 import Button from "./Button";
-import styles from "./App.module.css"
+import styles from "./App.module.css";
 import { useState, useEffect } from "react";
 
 function App() {
   const [counter, setValue] = useState(0);
-  const onClick = () => setValue((prev)=> prev+1);
-  console.log('i run all the time');
-  const iRunOnlyOnce = () => {
-    console.log("i run only once");
-  }
-  useEffect(iRunOnlyOnce, []);
+  const [keyword, setKeyword] = useState("");
+
+  const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
+
+  console.log("i run all the time");
+
+  useEffect(() => {
+    console.log("counter change");
+  }, [counter]);
+  useEffect(() => {
+    {
+      console.log("search for", keyword);
+    }
+  }, [keyword]);
+  useEffect(() => {
+    console.log("keyword n counter change");
+  }, [keyword, counter]);
+
   return (
     <div>
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search here..."
+      />
       <h1 className={styles.title}>{counter}</h1>
       <button onClick={onClick}>click me</button>
     </div>
